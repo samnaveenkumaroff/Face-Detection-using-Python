8cf588a7c5b4cff82f9af64d8e35533869326852
Sam Naveenkumar .V <143938999+samnaveenkumaroff@users.noreply.github.com>
2025-06-12 10:08:08 +0530

    Update code.py

--- FaceDetection/code.py
+++ FaceDetection/code.py
@@ -1,7 +1,7 @@
 import cv2
 import time
 
-# Initialize the face detection model
+# Initialize the face detection model, CascadeClassifier
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Check if model is loaded
